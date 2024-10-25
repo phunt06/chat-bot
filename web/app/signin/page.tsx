b0640a8ab7dc0ee33800c425e5dd94d335b6f65f
phunthopee <phunt@hopee.com.vn>
2024-10-25 22:58:19 +0700

    Update code

--- web/app/signin/page.tsx
+++ web/app/signin/page.tsx
@@ -75,7 +75,6 @@ gtag('config', 'AW-11217955271"');
             <>
               <Forms />
               <div className='px-8 py-6 text-sm font-normal text-gray-500'>
-                © {new Date().getFullYear()} LangGenius, Inc. All rights reserved.
               </div>
             </>
           )}
